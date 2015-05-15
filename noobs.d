1f9c3ac40442248d56e395cb12e6799a19dbc709
vuplea <vuplea.work@gmail.com>
2015-05-15 13:16:21 +0300

    using nailgun

--- noobs.d
+++ noobs.d
@@ -1,106 +1,91 @@
 #!/usr/bin/env rdmd
-import std.stdio, std.file, std.process, std.string, std.conv, std.algorithm,
+import std.stdio, std.file, std.process, std.string, std.conv, std.algorithm, std.getopt, core.thread,
        std.range, std.exception, std.typecons, std.path, std.random, std.parallelism, std.mathspecial;
 enum usage = `
-Usage: ./noobs.d bot1 bot2`;
+Usage: ./noobs.d bot1 bot2 [--count=<count>]`;
 
 
-string[string] load_config()
-{
-    enum defaults = [
-    "map_folder": "warmaps",
-    "warengine_folder": "warlight2-engine",
-    "warengine_command": `java -cp lib/java-json.jar:bin com.theaigames.game.warlight2.Warlight2 "%s" "%s" "%s" "%s"`
-    ];
-
-    string[string] ret;
-    try {
-        auto ret_str = (cast(string) File("noobs.conf").byChunk(4096).front) .filter!`a != '\n'`.array;
-        ret = ret_str.to!(string[string]);
-        foreach (key; setDifference(defaults.keys, ret.keys))
-            ret[key] = defaults[key];
-    }
-    catch (Exception e) {
-        if (cast(ErrnoException) e)
-            writeln("Could not open noobs.conf");
-        else
-            writeln("Could not parse config file");
-        ret = defaults;
-    }
-    return ret;
+struct settings {
+    string map_folder = "warmaps";
+    string warengine_folder = "warlight2-engine";
+    string warengine_command = `./ng com.theaigames.game.warlight2.Warlight2 "%s" "%s" "%s" "%s"`;
+    string nailgun_server_command = `java -Xmx256M -Xms256M -cp nailgun-server-0.9.2-SNAPSHOT.jar:bin:lib/java-json.jar com.martiansoftware.nailgun.NGServer`;
+    int count = int.max;
+    string bot1;
+    string bot2;
 }
 
-void test_execution(string[string] config)
+auto load_settings(string[] args)
 {
-    try {
-        auto ret = executeShell(config["warengine_command"].format("", "", "", ""),
-                null, Config.none, 4096, config["warengine_folder"]);
-        if (!canFind(ret.output, "parseLong"))
-            throw new Exception(ret.output);
-    }
-    catch (Exception e)
-        throw new Exception("Warengine test execution failed:\n" ~ e.msg);
-}
-
-auto get_bots(string[] args)
-{
+    settings ret;
+    getopt(args, "count", &ret.count);
     if (args.length != 3)
         throw new Exception("Invalid arguments" ~ usage);
-    Tuple!(string, "bot1", string, "bot2") ret;
     ret.bot1 = args[1];
     ret.bot2 = args[2];
     return ret;
 }
 
-void main(string[] args) {
-    auto bots = get_bots(args);
+void console_back_n_lines(int n)
+{
+    foreach (_; 0 .. n)
+        writef("%c[1A%c[2K", cast(char) 0x1B, cast(char) 0x1B);
+}
+
+void main(string[] args)
+{
+    auto settings = load_settings(args);
     string[] mapfiles;
-    auto config = load_config();
-    try mapfiles = dirEntries(config["map_folder"], SpanMode.shallow).map!"a.name".array;
-        catch throw new Exception("Could not open folder " ~ config["map_folder"]);
-    test_execution(config);
-    if (!bots.bot1.exists)
-        throw new Exception("File " ~ bots.bot1 ~ " does not exist");
-    if (!bots.bot2.exists)
-        throw new Exception("File " ~ bots.bot2 ~ " does not exist");
+    try mapfiles = dirEntries(settings.map_folder, SpanMode.shallow).map!"a.name".array;
+        catch throw new Exception("Could not open folder " ~ settings.map_folder);
+    if (!settings.bot1.exists)
+        throw new Exception("File " ~ settings.bot1 ~ " does not exist");
+    if (!settings.bot2.exists)
+        throw new Exception("File " ~ settings.bot2 ~ " does not exist");
 
     writeln("Available maps: " ~ mapfiles.length.to!string);
-    File err_log = File("errors.log", "wb");
 
-    int err_cnt, win1_cnt, game_cnt;
+    auto ngserver = pipeShell(settings.nailgun_server_command, Redirect.all,
+            null, Config.none, settings.warengine_folder);
+    scope(exit) kill(ngserver.pid);
+    if (!canFind(ngserver.stdout.byLine.front, "started on all interfaces"))
+        throw new Exception(cast(string) ngserver.stderr.byChunk(4096).front);
+    Thread.sleep(dur!"msecs"(800));
+    writeln("NGServer started");
+
+    int win1_cnt, game_cnt;
     bool first_time = true;
     writeln();
-    foreach (mapfile; taskPool.parallel(mapfiles, 10)) {
+    foreach (mapfile; taskPool.parallel(mapfiles.take(settings.count), 1))
+    {
         int seed = uniform(0, int.max);
-        foreach (swap; [false, true]) {
-            auto path1 = absolutePath(bots.bot1);
-            auto path2 = absolutePath(bots.bot2);
+        foreach (swap; [false, true])
+        {
+            auto path1 = absolutePath(settings.bot1);
+            auto path2 = absolutePath(settings.bot2);
             auto pathmap = absolutePath(mapfile);
-            auto command = config["warengine_command"]
+            auto command = settings.warengine_command
                 .format(pathmap, swap ? path2 : path1, swap ? path1 : path2, seed);
-            auto pipes = pipeShell(command, Redirect.all, null, Config.none,
-                    config["warengine_folder"]);
+            auto pipes = pipeShell(command, Redirect.all, null, Config.none, settings.warengine_folder);
+            wait(pipes.pid);
             auto command_output = cast(string) pipes.stdout.byChunk(4096).front;
             auto command_err = cast(string) pipes.stderr.byChunk(4096).front;
-            if (!canFind(command_output, "winner: ")) {
-                ++err_cnt;
-                err_log.writefln("Error in execution: %s\n%s\n", command, command_err);
-            } else {
-                ++game_cnt;
-                if ((canFind(command_output, "winner: player1") && !swap)
-                        || (canFind(command_output, "winner: player2") && swap))
-                    ++win1_cnt;
-            }
+            if (!canFind(command_output, "winner: "))
+                throw new Exception(command_err);
+
+            ++game_cnt;
+            if ((canFind(command_output, "winner: player1") && !swap)
+                    || (canFind(command_output, "winner: player2") && swap))
+                ++win1_cnt;
             auto win2_cnt = game_cnt - win1_cnt;
             if (first_time)
                 first_time = false;
             else
-                write("\r\b\r\b\r\b\r");
-            writefln("1st bot (%s): %s winrate %s/%s   ", bots.bot1,
+                console_back_n_lines(2);
+            writefln("1st bot (%s): %s winrate %s/%s", settings.bot1,
                     winrate(win1_cnt, game_cnt), win1_cnt, game_cnt);
-            writefln("2nd bot (%s): %s winrate %s/%s   ", bots.bot2,
+            writefln("2nd bot (%s): %s winrate %s/%s", settings.bot2,
                     winrate(win2_cnt, game_cnt), win2_cnt, game_cnt);
-            writefln("Error count: %s", err_cnt);
         }
     }
 }
